1464afbb89971f3c617b3adaf05ee8b48687422f
kyeongwonBang <ruddnjs8764@naver.com>
2025-09-23 15:16:28 +0900

    프로젝트 생성

---
+++ src/main/java/com/back/Nbe791Team02Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe791Team02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nbe791Team02Application.class, args);
+	}
+
+}
