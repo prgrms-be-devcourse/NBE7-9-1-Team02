b7232ac3f05b5da4719f6145c6faf8024b3752d8
zoooooz2 <92962527+zoooooz2@users.noreply.github.com>
2025-09-24 10:44:17 +0900

    feat(OrderListContoroller): 클래스 생성, 주문목록 조회 API설계 및 구현

---
+++ src/main/java/com/back/admin/OrderListController.java
@@ -0,0 +1,17 @@
+package com.back.admin;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/admin")
+public class OrderListController {
+
+    @GetMapping
+    public String adminOrderList() {
+        return "admin/orderList";
+    }
+}
